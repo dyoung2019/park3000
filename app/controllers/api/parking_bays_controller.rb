faae05b2a91f43e25770a48df0f7acbe4a25f682
David Young <david.young.email@gmail.com>
2019-11-28 00:50:18 +1100

    less field returned on /api/ParkingBays

--- app/controllers/api/parking_bays_controller.rb
+++ app/controllers/api/parking_bays_controller.rb
@@ -1,6 +1,24 @@
 class Api::ParkingBaysController < ApplicationController
+    def get_smaller_set()
+        records = ParkingBay.select(:marker_id).select(:the_geom).all
+
+        bays = records.map do | record |
+          bay = {}
+          bay[:marker_id] = record.marker_id unless !record.marker_id
+          bay[:the_geom] = record.the_geom.map do | point |
+            point.map do | num |
+              num.round(6)
+            end
+          end
+          bay
+        end
+        bays
+    end
+    
     def index
-        bays = ParkingBay.all
+        bays = get_smaller_set()
+
+        # bays = ParkingBay.all
         render json: bays
     end
 end
