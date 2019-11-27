0af9ce4c9a10233fcb28d16e8503f4d1fb669b7d
sunlee <sunlee128@gmail.com>
2019-11-27 17:07:59 +1100

    backend API set

--- app/models/parking_bay.rb
+++ app/models/parking_bay.rb
@@ -1,2 +1,3 @@
 class ParkingBay < ApplicationRecord
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+
   resources :parking_restrictions
   get '/api/ParkingRestrictions', to: 'api/parking_restrictions#index'
+
   resources :pay_stay_zones
-  # get '/api/PayStayZones/' 
+  get '/api/PayStayZones/', to: 'api/pay_stay_zones#index'
+
   resources :parking_bays
-  # get '/api/ParkingBays/'
-  
+  get '/api/ParkingBays', to: 'api/parking_bays#index'
   # get '/', to:'layouts#application' 
 end
-
